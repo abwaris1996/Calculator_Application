f519299caae1d7150c83036e733c310de4811778
abdulwariskhan <hellokhan.ch@gmail.com>
2023-05-28 19:39:30 +0200

    added ExpressionEvaluator.kt

--- app/src/main/java/com/myapplication/calculatorapplication/MainActivity.kt
+++ app/src/main/java/com/myapplication/calculatorapplication/MainActivity.kt
@@ -3,13 +3,6 @@ package com.myapplication.calculatorapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.myapplication.calculatorapplication.ui.theme.CalculatorApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +10,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalculatorApplicationTheme {
-        Greeting("Android")
     }
 }
