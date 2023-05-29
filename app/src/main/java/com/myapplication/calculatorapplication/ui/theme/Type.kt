0ba586f9393818adcd63d21664f14d8631328ca0
abdulwariskhan <hellokhan.ch@gmail.com>
2023-05-29 12:37:05 +0200

    Migrated Material2 to Material3 Added CalculatorUiAction.kt

--- app/src/main/java/com/myapplication/calculatorapplication/ui/theme/Type.kt
+++ app/src/main/java/com/myapplication/calculatorapplication/ui/theme/Type.kt
@@ -12,16 +12,5 @@ val Typography = androidx.compose.material3.Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+
 )
