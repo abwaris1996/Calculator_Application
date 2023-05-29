d268eaf5bc4c13983819b17a9e8801a5c43e96c1
abdulwariskhan <hellokhan.ch@gmail.com>
2023-05-29 12:55:51 +0200

    added MaterialCalculator Theme to main Activity

--- app/src/main/java/com/myapplication/calculatorapplication/ui/theme/Type.kt
+++ app/src/main/java/com/myapplication/calculatorapplication/ui/theme/Type.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = androidx.compose.material3.Typography(
     bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
