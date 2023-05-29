9c187bedcb180f938298c2ad2c92136980edd0e0
abdulwariskhan <hellokhan.ch@gmail.com>
2023-05-29 14:04:44 +0200

    Added UI

--- app/src/main/java/com/myapplication/calculatorapplication/MainActivity.kt
+++ app/src/main/java/com/myapplication/calculatorapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.myapplication.calculatorapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.myapplication.calculatorapplication.presentation.CalculatorScreen
 import com.myapplication.calculatorapplication.ui.theme.MaterialCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,9 +11,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialCalculatorTheme {
-
-
+                CalculatorScreen()
             }
         }
     }
 }
+
---
+++ app/src/main/java/com/myapplication/calculatorapplication/presentation/CalculatorViewModel.kt
@@ -0,0 +1,21 @@
+package com.myapplication.calculatorapplication.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.myapplication.calculatorapplication.domain.CalculatorAction
+import com.myapplication.calculatorapplication.domain.ExpressionWriter
+
+class CalculatorViewModel(
+    private val writer: ExpressionWriter = ExpressionWriter()
+): ViewModel() {
+
+    var expression by mutableStateOf("")
+        private set
+
+    fun onAction(action: CalculatorAction) {
+        writer.processAction(action)
+        this.expression = writer.expression
+    }
+}
